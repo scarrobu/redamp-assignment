b2a299477abe424401cd57445762ecd271dec70f
scarrobu <robert.burian.ml@gmail.com>
2023-10-29 08:50:24 +0100

    removed init command becoure was redundant, his function is implement in fill command

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 """
 Main file, usage:
-
-Start with:
-        main.py init  --  to create db
-        main.py load  --  to download data from web sources
-        main.py fill  --  to fill DB with transformed data
-
-After this, your database file is filled with data.
+    Start with:
+            main.py load  --  to download data from web sources
+            main.py fill  --  to create empty DB file and fill it with transformed data
+    After this, your database file is ready to examine.
 """
 import sys
 
@@ -17,15 +14,13 @@ from session_db import fill_db
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        if sys.argv[1] == 'init':
-            create_db()
-        elif sys.argv[1] == 'load':
+        if sys.argv[1] == 'load':
             load_data()
         elif sys.argv[1] == 'fill':
             open_file('d')
             fill_db()
         else:
-            print('Unknown argument, please use only: init|load|fill.')
+            print('Unknown argument, please use only: load|fill.')
 
     else:
         print(__doc__)
